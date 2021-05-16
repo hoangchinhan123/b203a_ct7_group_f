2ccda6583a0b32b0078e2ac622f3f0561039673d
ThanhHuy <phamthanhhuy332@gmail.com>
2021-05-16 18:14:08 +0700

    Add all my files

---
+++ resources/views/Banhang/Gioithieu.blade.php
@@ -0,0 +1,30 @@
+@extends("Banhang.master")
+@section("content")
+<div class="container">
+    <div class="row">
+        <div class="col-xs-12 col-sm-6 col-md-6">
+            <div class="well well-sm">
+                <div class="row">
+                    <div class="col-sm-3">
+                        <img src="images/logo.jpg" alt="" class="img-rounded img-responsive" />
+                    </div>
+                </div>
+                <br>
+                <div>
+        
+                        <h4>Nhóm 11</h4>
+                        <p>
+                            <i class="glyphicon glyphicon-envelope"></i>Lớp: CD18tt7
+                            <br />
+                            <i class="glyphicon glyphicon-globe"></i>Môn Học : Mã nguồn mở
+                            <br />
+                            <i class="glyphicon glyphicon-gift"></i>2021</p>
+                        <!-- Split button -->
+                
+                    </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+@endsection
