b8cb15a954649fd50d7759acc2412b3c283cefa3
nhan <chinhanhoan@gmail.com>
2022-06-09 23:58:18 +0700

    cap nhat ten data

--- resources/views/Banhang/sanpham/search.blade.php
+++ resources/views/Banhang/sanpham/search.blade.php
@@ -11,7 +11,7 @@
                                             <img src="{{URL::to('uploads/product/'.$product->product_image)}}" alt="" />
                                             <h2>{{number_format($product->product_price).' '.'VNĐ'}}</h2>
                                             <p>{{$product->product_name}}</p>
-                                            <a href="#" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Thêm giỏ hàng</a>
+                                            
                                         </div>
 
                                 </div>
